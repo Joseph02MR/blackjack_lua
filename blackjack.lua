3c4c269f0c76a4feb3b11ddd27000cb9861cc1c6
Joseph02MR <josmael0202@hotmail.com>
2022-08-11 20:20:42 -0500

    better drawing and initial game rules, action btns

--- blackjack.lua
+++ blackjack.lua
@@ -7,38 +7,51 @@
 -- script:  lua
 
 function BOOT()
-	t=0
-	drawn=false
---	x=80
---	y=80
-	sw=8  --spr width
-	cs=30 --card space
-	css=18 --card short space
+	T=0
+	DRAWN=false
+	SW=8  --spr width
+	CS=30 --card space
+	CSS=18 --card short space
 
-	xb=160 --coordinates for initial button
-	yb=110
+	XB=160 --coordinates for initial button
+	YB=110
 
-	pcards={} --play cards
-	deck_ptr=1
+	PCARDS={} --play cards
+	local deck_ptr=1
+	DNUM=0
 	for i=1,4 do
 		for j=1,13 do
-			pcards[deck_ptr]={i,j}
+			PCARDS[deck_ptr]={i,j}
 			deck_ptr=deck_ptr+1
 		end
 	end
-	played={}
-	played_ptr=0
+	PDECK={}
 
-	dealer={}
-	player={}
+	PLAYED={}
+	PLAYED_PTR=0
 
-	dheight=5 --dealer card height
-	pheight=60 --players card heigth
+	DEALER={}
+	PLAYER={}
+
+	P_BJ=false --blackjack flags
+	D_BJ=false
+
+	P_BUSTED=false --busted flags
+	D_BUSTED=false
+
+	CURRENT_CARD=0
+
+	D_HEIGHT=5 --dealer card height
+	P_HEIGHT=60 --players card heigth
 
 	--TIC AUX
-	dealt=0
-	dcnt=0
-	show=false
+	DEALT=0
+	GAME_END=false
+	DEALER_SHOW=false
+	ACTIONABLE=false
+	ACTIVE_BTNS={0,0,0,0}
+	EVAL=false
+	RESULT=""
 end
 
 
@@ -64,6 +77,7 @@ function create_deck(deck_num)
 		k=k+1
 	until(deck_num==k)
 	shuffle(aux,#aux)
+	DNUM=deck_num
 	return aux
 end
 
@@ -76,94 +90,184 @@ function sel_tile(x,y)
 	end
 end
 
-function draw_card(deck,card,x,y)
+function draw_card(card,x,y)
 	for i=0,3,1	do
 		for j=0,2,1 do
-			spr(sel_tile(j,i),(sw*j)+x,(sw*i)+y,0,1,0,0,1,1)
+			spr(sel_tile(j,i),(SW*j)+x,(SW*i)+y,0,1,0,0,1,1)
 		end
 	end
 
- local c=31+deck[card][1]
- local n=47+deck[card][2]
-	spr(c,(sw+x),(sw+y) ,0,1,0,0,1,1);
-	spr(n,(sw+x),(sw*2)+y+1,0,1,0,0,1,1);
+ 	local c=31+PCARDS[card][1]
+ 	local n=47+PCARDS[card][2]
+	spr(c,(SW+x),(SW+y) ,0,1,0,0,1,1);
+	spr(n,(SW+x),(SW*2)+y+1,0,1,0,0,1,1);
 end
 
 function draw_back(x,y)
 	local aux=0
 	for i=0,3,1	do
 		for j=0,2,1 do
-			spr(16+aux,(sw*j)+x,(sw*i)+y,0,1,0,0,1,1)
+			spr(16+aux,(SW*j)+x,(SW*i)+y,0,1,0,0,1,1)
 			aux=aux+1
 		end
 	end
 end
 
 function update_played(card,owner)
-	played_ptr=played_ptr+1
-	played[played_ptr] = {card,owner}
+	PLAYED_PTR=PLAYED_PTR+1
+	PLAYED[PLAYED_PTR] = {card,owner}
 end
 
-function first_deal(deck)
+function first_deal()
 	local card=1
 	for cn=1,2 do
-		player[cn]=deck[card]
-		update_played(player[cn],1)
-		deck[card]=0
+		PLAYER[cn]=PDECK[card]
+		update_played(PLAYER[cn],1)
+		PDECK[card]=0
 		card=card+1
-		dealer[cn]=deck[card]
-		update_played(dealer[cn],0)
-		deck[card]=0
+		DEALER[cn]=PDECK[card]
+		update_played(DEALER[cn],0)
+		PDECK[card]=0
 		card=card+1
 	end
-
 	return card
 end
 
-function draw(cards, show)
-	--[[for cn=1,2 do
-		for i=1,#players do
-			draw_card(pcards, players[i][cn],80+(cn-1)*css,pheight)
-		end
-		draw_card(pcards, dealer[cn],80+(cn-1)*cs,dheight)
+function deal(owner, card)
+	local aux=-1
+	owner[#owner+1]=PDECK[card]
+	if owner==PLAYER then aux=1
+	else aux=0
 	end
-	--]]
+	update_played(owner[#owner],aux)
+	card=card+1
+	return card
+end
+
+function draw(cards, DEALER_SHOW)
+	local increment=0
+	local pinc=0
+	local dinc=0
 	for i=1, cards do
-		if i==4 and not show then
-			draw_back(80+3*css,dheight)
+		if i==4 and not DEALER_SHOW then
+			draw_back(80+(increment)*CSS,D_HEIGHT)
 		else
-			if played[i][2]~=0 then draw_card(pcards, played[i][1],80+(i-1)*css,pheight)
-			else draw_card(pcards, played[i][1],80+(i-1)*css,dheight)
+			if PLAYED[i][2]~=0 then
+				if i>4 then pinc=pinc+1 end
+				draw_card(PLAYED[i][1],80+(increment+pinc)*CSS,P_HEIGHT)
+			else
+				if i>4 then dinc=dinc+1 end
+				draw_card(PLAYED[i][1],80+(increment+dinc)*CSS,D_HEIGHT)
 			end
 		end
+		if i%2==0 and i<=4 then increment=increment+1 end
 	end
 end
 
-function draw_buttons(double,split)
+function draw_buttons()
+	local aux=0
 	for i=0, 3 do
-		spr(5+i,xb-(i*34),yb,0,2,0,0,1,1)
-		line(xb+2-(i*34),yb-2,xb+13-(i*34),yb-2,8)
-		line(xb+2-(i*34),yb-1,xb+13-(i*34),yb-1,8)
+		if ACTIVE_BTNS[i+1]==i+1 then aux=5 else aux=9 end
+		spr(aux+i,XB-(i*34),YB,0,2,0,0,1,1)
+		line(XB+2-(i*34),YB-2,XB+13-(i*34),YB-2,8)
+		line(XB+2-(i*34),YB-1,XB+13-(i*34),YB-1,8)
 	end
 end
+
+function eval_count(owner)
+	local id=-1
+	if owner==PLAYER then id=1 else id=0 end
+	local pcnt=0
+	local aux=0
+	local aces=0
+	for i=1,#owner do
+		aux=PCARDS[owner[i]][2]
+		if aux >=2 and aux <=10 then pcnt=pcnt+aux
+		elseif aux==11 or aux==12 or aux==13 then pcnt=pcnt+10
+		else aces=aces+1
+		end
+	end
+	if aces>0 then
+		local add=0
+		for i=1, aces do
+			if pcnt+(i+10)<=21 then add=i+10 else add=i end
+		end
+		pcnt=pcnt+add
+	end
+	if pcnt==21 and #owner==2 then
+		if id==1 then P_BJ=true else D_BJ=true end
+	end
+	if pcnt>21 then
+		if id==1 then P_BUSTED=true else D_BUSTED=true end
+	end
+	return pcnt
+end
+
+function dealer_play()
+	local dcnt=eval_count(DEALER)
+	if dcnt<=16 then
+		deal(DEALER,CURRENT_CARD)
+		dcnt=eval_count(DEALER)
+		DEALT=DEALT+1
+	else EVAL=true
+	end
+end
+
+function eval_options()
+	if GAME_END then ACTIVE_BTNS = {0,0,0,0} else
+		local pcnt=eval_count(PLAYER)
+		if pcnt<21 then ACTIVE_BTNS[1]=1 else ACTIVE_BTNS[1]=0 end --hit
+		if pcnt<21 then ACTIVE_BTNS[4]=4 else ACTIVE_BTNS[4]=0 end --stand
+		if #PLAYER==2 then ACTIVE_BTNS[2]=2 else ACTIVE_BTNS[2]=0 end --double
+		if PCARDS[PLAYER[1]][2]==PCARDS[PLAYER[2]][2] then ACTIVE_BTNS[3]=3 else ACTIVE_BTNS[3]=0 end --split
+	end
+end
+
 
 function draw_selected()
-	local mx,my,ml=mouse()
+	local mx,my=mouse()
 	local x_in=false
 	local y_in=false
 	local b=-1
 	for i=0, 3 do
-		if(xb-(i*34) < mx and mx < xb-(i*34)+16) then x_in=true
-			if(yb < my and my < yb+16) then
+		if(XB-(i*34) < mx and mx < XB-(i*34)+16) then x_in=true
+			if(YB < my and my < YB+16) then
 				y_in=true
 				b=i
 			end
 		end
 	end
 	if y_in and x_in then
-		spr(12,xb-(b*34)-2,yb-4,0,2,0,0,2,2)
-		if ml then button_click(b) end
+		if ACTIVE_BTNS[b+1]==b+1 then spr(14,XB-(b*34)-2,YB-4,0,2,0,0,2,2) end
 	end
+end
+
+function eval_click()
+	local mx,my,ml=mouse()
+	local x_in=false
+	local y_in=false
+	local b=-1
+	for i=0, 3 do
+		if(XB-(i*34) < mx and mx < XB-(i*34)+16) then x_in=true
+			if(YB < my and my < YB+16) then
+				y_in=true
+				b=i
+			end
+		end
+	end
+	if y_in and x_in then
+		if ml and ACTIONABLE and not GAME_END then button_click(b); ACTIONABLE=false; eval_options() end
+	end
+	if not ml then ACTIONABLE=true end
+end
+
+function eval_pressed() --falta corregir multiaccion
+	if btn(4) and ACTIONABLE then hit();ACTIONABLE=false;
+	elseif btn(5) and ACTIONABLE then stand(); ACTIONABLE=false;
+	elseif btn(6) and ACTIONABLE then double(); ACTIONABLE=false;
+	elseif btn(7) and ACTIONABLE then split(); ACTIONABLE=false;
+	end
+	if not (btn(4) or btn(5) or btn(6) or btn(7)) then ACTIONABLE=true end
 end
 
 function button_click(b)
@@ -176,6 +280,8 @@ end
 
 function hit()
 	print("hit",0,130)
+	CURRENT_CARD=deal(PLAYER,CURRENT_CARD)
+	DEALT=DEALT+1
 end
 
 function double()
@@ -188,57 +294,82 @@ end
 
 function stand()
 	print("stand",0,130)
-	show=true
+	GAME_END=true
+	DEALER_SHOW=true
+end
+
+function eval_game(pcnt, dcnt)
+	if P_BUSTED or (D_BJ and not P_BJ) or dcnt>pcnt then RESULT="LOSE" end
+	if D_BUSTED or (P_BJ and not D_BJ) or pcnt>dcnt then RESULT="WIN"
+	elseif pcnt==dcnt or (P_BJ and D_BJ) then RESULT="PUSH"
+	end
+	return RESULT
 end
 
 function TIC()
 	cls(6)
-	if not drawn then
-	deck=create_deck(2)
-	dcnt=first_deal(deck)
-	drawn = true
+	if not DRAWN then
+	PDECK=create_deck(2)
+	CURRENT_CARD=first_deal()
+	DRAWN = true
 	end
 
-	if t%30==0 and dealt<4 then
-		dealt=dealt+1
+	if T%30==0 and DEALT<=4 then
+		if DEALT==4 then ACTIONABLE=true; eval_options()
+		else DEALT=DEALT+1
+		end
 	end
-	draw(dealt,show)
+
+	draw(DEALT,DEALER_SHOW)
 
 	draw_selected()
 	draw_buttons()
+	eval_click()
+	--eval_pressed()
+	local pcnt=eval_count(PLAYER)
+	local dcnt=eval_count(DEALER)
 
-	t=t+1
-	print(dealt,230,0)
-	--debug
-	--[[
-	draw_card(pcards,deck[1],x,y)
-	line(x+17,y,x+17,y+31,13)
-	draw_card(pcards,deck[2],x+18,y)
-	
-	draw_back(x,y-50)
-	draw_back(x+cs,y-50)
-	--]]
+	print(pcnt, 65,P_HEIGHT)
+	--print(dcnt, 70,D_HEIGHT)
+
+	if GAME_END and T%30==0 then
+		dealer_play()
+	end
+
+	if EVAL then
+		print(eval_game(pcnt,dcnt), 210,130)
+	end
+
+
+	T=T+1
+	print(DEALT,230,0)
+	--print(ACTIONABLE,230,124)
 
 --[[ debug-check shuffle	
 	aux=0
 	z=0
-	for i=1,#deck do
-		print(deck[i],z,aux)
+	for i=1,#PDECK do
+		print(PDECK[i],z,aux)
 		aux=aux+6
 		if aux>120 then z=z+20;aux=0 end
 	end
 	--]]
 	local aux2=0
-	for i=1,#player do
-		print(pcards[player[i]][1].. " ".. pcards[player[i]][2],0,aux2)
+	for i=1,#PLAYER do
+		print(PCARDS[PLAYER[i]][1].. " ".. PCARDS[PLAYER[i]][2],0,aux2)
 		aux2=aux2+6
 	end
 	print("dealer",0,aux2)
 	aux2=aux2+6
-	for i=1,#dealer do
-		print(pcards[dealer[i]][1].." "..pcards[dealer[i]][2],0,aux2)
+	for i=1,#DEALER do
+		print(PCARDS[DEALER[i]][1].." "..PCARDS[DEALER[i]][2],0,aux2)
 		aux2=aux2+6
 	end
+
+	print(D_BUSTED,0,100)
+	print(P_BUSTED,0,106)
+	print(D_BJ,0,112)
+	print(P_BJ,0,118)
 end
 
 --14390
@@ -259,10 +390,12 @@ end
 -- 006:8aaaaaa88a888aa88a8aa8a88a8aa8a88a8aa8a88a888aa88aaaaaa808888880
 -- 007:8488444884844448844844488488884884448848844484488444444808888880
 -- 008:8222222882888828828222288288882882222828828888288222222808888880
--- 009:8999999889888998898998988989989889899898898889988999999808888880
--- 010:8b88bbb88b8bbbb88bb8bbb88b8888b88bbb88b88bbb8bb88bbbbbb808888880
--- 012:00cccccc0c000000c0000000c0000000c0000000c0000000c0000000c0000000
--- 013:00000000c00000000c0000000c0000000c0000000c0000000c0000000c000000
+-- 009:8777777887877878878778788788887887877878878778788777777808888880
+-- 010:8999999889888998898998988989989889899898898889988999999808888880
+-- 011:8b88bbb88b8bbbb88bb8bbb88b8888b88bbb88b88bbb8bb88bbbbbb808888880
+-- 012:8111111881888818818111188188881881111818818888188111111808888880
+-- 014:00cccccc0c000000c0000000c0000000c0000000c0000000c0000000c0000000
+-- 015:00000000c00000000c0000000c0000000c0000000c0000000c0000000c000000
 -- 016:0ccccccccc444444c4322222c4234444c4243222c4242344c4242432c4242423
 -- 017:cccccccc44444444222222224444444422222222444444442222222244444444
 -- 018:ccccccc0444444cc2222234c4444324c2223424c4432424c2342424c3242424c
@@ -275,8 +408,8 @@ end
 -- 025:c4242423c4242432c4242344c4243222c4234444c4322222cc4444440ccccccc
 -- 026:44444444222222224444444422222222444444442222222244444444cccccccc
 -- 027:3242424c2342424c4432424c2223424c4444324c2222234c444444ccccccccc0
--- 028:c00000000c00000000cccccc0000000000000000000000000000000000000000
--- 029:0c000000c0000000000000000000000000000000000000000000000000000000
+-- 030:c00000000c00000000cccccc0000000000000000000000000000000000000000
+-- 031:0c000000c0000000000000000000000000000000000000000000000000000000
 -- 032:000ff00000ffff000ffffff0ffffffffffffffffffffffffff0ff0ff00ffff00
 -- 033:000ff00000ffff0000ffff000f0ff0f0ffffffffffffffff0f0ff0f000ffff00
 -- 034:0220022022222222222222222222222222222222022222200022220000022000
